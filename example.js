25ca4e061bb690a0400b1b6b5feba5f5a3046137
Weston Ruter <westonruter@gmail.com>
2009-10-05 23:36:54 -0700

    Starting out with recursion

---
+++ example.js
@@ -0,0 +1,258 @@
+
+// This code was written by Tyler Akins and has been placed in the
+// public domain.  It would be nice if you left this header intact.
+// Base64 code from Tyler Akins -- http://rumkin.com
+
+var keyStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
+
+function encode64(input) {
+   var output = "";
+   var chr1, chr2, chr3;
+   var enc1, enc2, enc3, enc4;
+   var i = 0;
+
+   do {
+      chr1 = input.charCodeAt(i++);
+      chr2 = input.charCodeAt(i++);
+      chr3 = input.charCodeAt(i++);
+
+      enc1 = chr1 >> 2;
+      enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);
+      enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);
+      enc4 = chr3 & 63;
+
+      if (isNaN(chr2)) {
+         enc3 = enc4 = 64;
+      } else if (isNaN(chr3)) {
+         enc4 = 64;
+      }
+
+      output = output + keyStr.charAt(enc1) + keyStr.charAt(enc2) + 
+         keyStr.charAt(enc3) + keyStr.charAt(enc4);
+   } while (i < input.length);
+   
+   return output;
+}
+
+function decode64(input) {
+   var output = "";
+   var chr1, chr2, chr3;
+   var enc1, enc2, enc3, enc4;
+   var i = 0;
+
+   // remove all characters that are not A-Z, a-z, 0-9, +, /, or =
+   input = input.replace(/[^A-Za-z0-9\+\/\=]/g, "");
+
+   do {
+      enc1 = keyStr.indexOf(input.charAt(i++));
+      enc2 = keyStr.indexOf(input.charAt(i++));
+      enc3 = keyStr.indexOf(input.charAt(i++));
+      enc4 = keyStr.indexOf(input.charAt(i++));
+
+      chr1 = (enc1 << 2) | (enc2 >> 4);
+      chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);
+      chr3 = ((enc3 & 3) << 6) | enc4;
+
+      output = output + String.fromCharCode(chr1);
+
+      if (enc3 != 64) {
+         output = output + String.fromCharCode(chr2);
+      }
+      if (enc4 != 64) {
+         output = output + String.fromCharCode(chr3);
+      }
+   } while (i < input.length);
+
+   return output;
+}
+
+/***** From jsolait library: lgpl license *****************/
+
+// LZW-compress a string
+function lzw_encode(s) {
+    var dict = {};
+    var data = (s + "").split("");
+    var out = [];
+    var currChar;
+    var phrase = data[0];
+    var code = 256;
+    for (var i=1; i<data.length; i++) {
+        currChar=data[i];
+        if (dict[phrase + currChar] != null) {
+            phrase += currChar;
+        }
+        else {
+            out.push(phrase.length > 1 ? dict[phrase] : phrase.charCodeAt(0));
+            dict[phrase + currChar] = code;
+            code++;
+            phrase=currChar;
+        }
+    }
+    out.push(phrase.length > 1 ? dict[phrase] : phrase.charCodeAt(0));
+    for (var i=0; i<out.length; i++) {
+        out[i] = String.fromCharCode(out[i]);
+    }
+    return out.join("");
+}
+
+// Decompress an LZW-encoded string
+function lzw_decode(s) {
+    var dict = {};
+    var data = (s + "").split("");
+    var currChar = data[0];
+    var oldPhrase = currChar;
+    var out = [currChar];
+    var code = 256;
+    var phrase;
+    for (var i=1; i<data.length; i++) {
+        var currCode = data[i].charCodeAt(0);
+        if (currCode < 256) {
+            phrase = data[i];
+        }
+        else {
+           phrase = dict[currCode] ? dict[currCode] : (oldPhrase + currChar);
+        }
+        out.push(phrase);
+        currChar = phrase.charAt(0);
+        dict[code] = oldPhrase + currChar;
+        code++;
+        oldPhrase = phrase;
+    }
+    return out.join("");
+}
+
+
+
+//From http://www.geocities.com/diogok_br/lz77/lz77.js
+
+ReferencePrefix = "`";
+ReferencePrefixCode = ReferencePrefix.charCodeAt(0);
+
+ReferenceIntBase = 96;
+ReferenceIntFloorCode = " ".charCodeAt(0);
+ReferenceIntCeilCode = ReferenceIntFloorCode + ReferenceIntBase - 1;
+
+MaxStringDistance = Math.pow(ReferenceIntBase, 2) - 1;
+MinStringLength = 5;
+MaxStringLength = Math.pow(ReferenceIntBase, 1) - 1 + MinStringLength;
+
+MaxWindowLength = MaxStringDistance + MinStringLength;
+
+function encodeReferenceInt(value, width) {
+  if ((value >= 0) && (value < (Math.pow(ReferenceIntBase, width) - 1))) {
+    var encoded = "";
+    while (value > 0) {
+      encoded = (String.fromCharCode((value % ReferenceIntBase) + ReferenceIntFloorCode)) + encoded;
+      value = Math.floor(value / ReferenceIntBase);
+    }
+
+    var missingLength = width - encoded.length;
+    for (var i = 0; i < missingLength; i++) {
+      encoded = String.fromCharCode(ReferenceIntFloorCode) + encoded;
+    }
+
+    return encoded;
+  } else {
+    throw "Reference int out of range: " + value + " (width = " + width + ")";
+  }
+}
+
+function encodeReferenceLength(length) {
+  return encodeReferenceInt(length - MinStringLength, 1);
+}
+
+function decodeReferenceInt(data, width) {
+  var value = 0;
+  for (var i = 0; i < width; i++) {
+    value *= ReferenceIntBase;
+    var charCode = data.charCodeAt(i);
+    if ((charCode >= ReferenceIntFloorCode) && (charCode <= ReferenceIntCeilCode)) {
+      value += charCode - ReferenceIntFloorCode;
+    } else {
+      throw "Invalid char code in reference int: " + charCode;
+    }
+  }
+  return value;
+}
+
+function decodeReferenceLength(data) {
+  return decodeReferenceInt(data, 1) + MinStringLength;
+}
+
+function compress(data, windowLength) {
+  if (windowLength > MaxWindowLength) {
+    throw "Window length too large";
+  }
+
+  var compressed = "";
+  var pos = 0;
+  var lastPos = data.length - MinStringLength;
+  while (pos < lastPos) {
+    var searchStart = Math.max(pos - windowLength, 0);
+    var matchLength = MinStringLength;
+    var foundMatch = false;
+    var bestMatch = {distance: MaxStringDistance, length: 0};
+    var newCompressed = null;
+
+    while ((searchStart + matchLength) < pos) {
+      var isValidMatch = (
+        (data.substr(searchStart, matchLength) == data.substr(pos, matchLength))
+        && (matchLength < MaxStringLength)
+      );
+      if (isValidMatch) {
+        matchLength++;
+        foundMatch = true;
+      } else {
+        var realMatchLength = matchLength - 1;
+        if (foundMatch && (realMatchLength > bestMatch.length)) {
+          bestMatch.distance = pos - searchStart - realMatchLength;
+          bestMatch.length = realMatchLength;
+        }
+        matchLength = MinStringLength;
+        searchStart++;
+        foundMatch = false;
+      }
+    }
+
+    if (bestMatch.length) {
+      newCompressed = ReferencePrefix
+        + encodeReferenceInt(bestMatch.distance, 2)
+        + encodeReferenceLength(bestMatch.length);
+      pos += bestMatch.length;
+    } else {
+      if (data.charAt(pos) != ReferencePrefix) {
+        newCompressed = data.charAt(pos);
+      } else {
+        newCompressed = ReferencePrefix + ReferencePrefix;
+      }
+      pos++;
+    }
+
+    compressed += newCompressed;
+  }
+  return compressed + data.slice(pos).replace(/`/g, "``");
+}
+
+function decompress(data) {
+  var decompressed = "";
+  var pos = 0;
+  while (pos < data.length) {
+    var currentChar = data.charAt(pos);
+    if (currentChar != ReferencePrefix) {
+      decompressed += currentChar;
+      pos++;
+    } else {
+      var nextChar = data.charAt(pos + 1);
+      if (nextChar != ReferencePrefix) {
+        var distance = decodeReferenceInt(data.substr(pos + 1, 2), 2);
+        var length = decodeReferenceLength(data.charAt(pos + 3));
+        decompressed += decompressed.substr(decompressed.length - distance - length, length);
+        pos += MinStringLength - 1;
+      } else {
+        decompressed += ReferencePrefix;
+        pos += 2;
+      }
+    }
+  }
+  return decompressed;
+}
--- svg-tree-drawer.js
+++ svg-tree-drawer.js
@@ -2,9 +2,192 @@
  * SVG Tree Drawer
  * by Weston Ruter
  *
- * 
+ * Initially built for Syntax Tree Diagrammer
  */
 (function(){
+if(typeof TreeDrawer != 'undefined')
+	return;
+if(typeof svgweb == 'undefined')
+	throw Error("Requires the use of svgweb");
+
+/**
+ * Class that is associated with a given SVG element and contains methods and
+ * properties that relate to the tree as a whole.
+ */
+var T = window.TreeDrawer = function(svgContainerElement, treeData, options){
+	if(!options)
+		options = {};
+	
+	if(typeof svgContainerElement == 'string')
+		svgContainerElement = document.getElementById(svgContainerElement);
+	if(!svgContainerElement || !svgContainerElement.nodeType == 1)
+		throw Error("The param 'svgContainerElement' is not valid.");
+	
+	//if(options.padding)
+	//	s.padding = 10;
+	
+	// Create the SVG document
+	var obj = document.createElement('object', true);
+	obj.setAttribute('type', 'image/svg+xml');
+	obj.setAttribute('data', 'data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg"></svg>');
+	obj.setAttribute('width', 0);
+	obj.setAttribute('height', 0);
+	this.svgObject = obj;
+	var that = this;
+	obj.addEventListener('load', function(e){
+		if(treeData)
+			that.populate(treeData);
+		
+		//obj.width = 100;
+		//obj.height = 100;
+		//var circle = document.createElementNS(svgns, 'circle');
+		//circle.setAttribute('cx', 50);
+		//circle.setAttribute('cy', 50);
+		//circle.setAttribute('r', 50);
+		//circle.setAttribute('fill', 'red');
+		//obj.contentDocument.documentElement.appendChild(circle);
+		
+	}, false);
+	svgweb.appendChild(obj, svgContainerElement);
+
+	//obj.setAttribute('data', 'data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg"></svg>');
+	
+	
+};
+
+T.prototype.svgObject = null;
+T.prototype.collapsed = false;
+
+//These two following should be set with CSS
+T.prototype.branchStyle = {
+	height:30
+};
+T.prototype.labelStyle = {
+	fontSize:20,
+	padding:10
+};
+
+
+/**
+ * The root TreeDrawer.Node
+ */
+T.prototype.root = null;
+
+
+/**
+ * Empty the tree
+ */
+T.prototype.empty = function empty(){
+	var svg = this.svgObject.contentDocument.documentElement;
+	for(var i = 0; i < svg.childNodes.length; i++){
+		if(svg.childNodes[i].nodeName.toLowerCase() == 'g'){
+			svg.removeChild(svg.childNodes[i]);
+			i--;
+		}
+	}
+}
+
+
+/**
+ * Renders the tree onto the SVG canvas, resizing the canvas as necessary
+ * This function does the heavy lifting of the code
+ */
+T.prototype.draw = function draw(){
+	this.empty();
+	var svg = this.svgObject.contentDocument.documentElement;
+	var info = _drawNode(this, svg, this.root, 0, 0, 0);
+	this.svgObject.width = info.width;
+	this.svgObject.height = this.labelStyle.fontSize;
+	console.info(info)
+};
+
+
+/**
+ * Recursive function called by TreeDrawer.draw()
+ */
+function _drawNode(tree, parentElement, treeNode, offsetLeft, offsetTop, parentWidth){
+	
+	var g = document.createElementNS(svgns, 'g');
+	var label = document.createElementNS(svgns, 'text');
+	label.appendChild(document.createTextNode(treeNode.label, true));
+	label.style.fontSize = tree.labelStyle.fontSize + 'px';
+	g.appendChild(label);
+	label.setAttribute('y', tree.labelStyle.fontSize);
+	
+	var branch;
+	if(parentElement.nodeName != 'svg'){
+		branch = document.createElementNS(svgns, 'line');
+		g.appendChild(branch);
+	}
+	
+	parentElement.appendChild(g);
+	//console.info(label.getComputedTextLength())
+	
+	
+	
+	//console.info(parentElement, treeNode, g)
+	return {
+		label:label,
+		branch:branch,
+		width:label.getComputedTextLength()
+	};
+}
+
+
+/**
+ * Populate the SVG tree with data, overriding the existing data
+ * @param {object} treeData  A instance of TreeDrawer.Node
+ */
+T.prototype.populate = function populate(treeNode){
+	if(treeNode instanceof TN)
+		this.root = treeNode;
+	else
+		this.root = new TN(treeNode);
+	this.draw();
+};
+
+
+/**
+ * Class that represents a node in a tree
+ */
+var TN = T.Node = function(obj){
+	this.label = obj.label;
+	this.collapsed = !!obj.collapsed;
+	if(obj.children && obj.children.length){
+		this.children = [];
+		for(var i = 0, len = obj.children.length; i < len; i++){
+			this.children.push(new TN(obj.children[i]));
+		}
+		//console.info(this.children)
+	}
+};
+TN.prototype.label = "";
+TN.prototype.collapsed = false;
+TN.prototype.children = [];
+
+
+/**
+ * If the node is collapsed, then expand it and draw.
+ */
+TN.prototype.expand = function expand(){
+	if(!this.collapsed)
+		return;
+	this.collapsed = false;
+	this.draw();
+};
+
+
+/**
+ * If the node is expanded, then collapse it and draw.
+ */
+TN.prototype.collapse = function collapse(){
+	if(this.collapsed)
+		return;
+	this.collapsed = true;
+	this.draw();
+};
+
+
 
 /*
 1. Convert SVG to Canvas via standard canvas API informed by positions and dimensions 
@@ -18,242 +201,9 @@
  
 NOTE: Must be valid JSON, otherwise someone could inject some bad JavaScript in a bad URL
 QUESTION: Can we do Packer without the self-extraction code included? We can do a JavaScript implementation of GZip and then store result in hash after Base64
-{
-	'label':'S',
-	'children':[
-		{
-			'label':'NP',
-			'children':[
-				{
-					'label':'N',
-					'children':[
-						{
-							'label':"boy"
-						}
-					],
-					'collapsed':false
-				}
-			],
-			'collapsed':true
-		}
-	],
-	'collapsed':false
-}
+
 
 */
 
-var example = {
-	'label':'S',
-	'children':[
-		{
-			'label':'NP',
-			'children':[
-				{
-					'label':'N',
-					'children':[
-						{
-							'label':"boy"
-						}
-					],
-					'collapsed':false
-				}
-			],
-			'collapsed':true
-		},
-		{
-			'label':'NP',
-			'children':[
-				{
-					'label':'N',
-					'children':[
-						{
-							'label':"boy"
-						}
-					],
-					'collapsed':false
-				}
-			],
-			'collapsed':true
-		},
-		{
-			'label':'NP',
-			'children':[
-				{
-					'label':'N',
-					'children':[
-						{
-							'label':"boy"
-						}
-					],
-					'collapsed':false
-				}
-			],
-			'collapsed':true
-		}
-	],
-	'collapsed':false
-};
-
-window.addEventListener('load', function(e){
-
-	//console.info(JSON.stringify(example).length)
-	//console.info(encode64(lzw_encode(JSON.stringify(example))).length)
-	//console.info(lzw_decode(lzw_encode(JSON.stringify(example))))
-
-	
-	
-	// note the extra 'true' argument
-	//var frag = document.createDocumentFragment(true);
-	//for (var i = 0; i < 100; i++) {
-	  var circle = document.createElementNS(svgns, 'circle');
-	  circle.setAttribute('cx', 100);
-	  circle.setAttribute('cy', 100);
-	  circle.setAttribute('r', 50);
-	  circle.setAttribute('fill', 'red');
-	  // append to DocumentFragment
-	  //frag.appendChild(circle);
-	//}
-	
-	var svg = document.getElementsByTagNameNS(svgns, 'svg')[0];
-	svg.appendChild(circle);
-
-
-}, false)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// This code was written by Tyler Akins and has been placed in the
-// public domain.  It would be nice if you left this header intact.
-// Base64 code from Tyler Akins -- http://rumkin.com
-
-var keyStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
-
-function encode64(input) {
-   var output = "";
-   var chr1, chr2, chr3;
-   var enc1, enc2, enc3, enc4;
-   var i = 0;
-
-   do {
-      chr1 = input.charCodeAt(i++);
-      chr2 = input.charCodeAt(i++);
-      chr3 = input.charCodeAt(i++);
-
-      enc1 = chr1 >> 2;
-      enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);
-      enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);
-      enc4 = chr3 & 63;
-
-      if (isNaN(chr2)) {
-         enc3 = enc4 = 64;
-      } else if (isNaN(chr3)) {
-         enc4 = 64;
-      }
-
-      output = output + keyStr.charAt(enc1) + keyStr.charAt(enc2) + 
-         keyStr.charAt(enc3) + keyStr.charAt(enc4);
-   } while (i < input.length);
-   
-   return output;
-}
-
-function decode64(input) {
-   var output = "";
-   var chr1, chr2, chr3;
-   var enc1, enc2, enc3, enc4;
-   var i = 0;
-
-   // remove all characters that are not A-Z, a-z, 0-9, +, /, or =
-   input = input.replace(/[^A-Za-z0-9\+\/\=]/g, "");
-
-   do {
-      enc1 = keyStr.indexOf(input.charAt(i++));
-      enc2 = keyStr.indexOf(input.charAt(i++));
-      enc3 = keyStr.indexOf(input.charAt(i++));
-      enc4 = keyStr.indexOf(input.charAt(i++));
-
-      chr1 = (enc1 << 2) | (enc2 >> 4);
-      chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);
-      chr3 = ((enc3 & 3) << 6) | enc4;
-
-      output = output + String.fromCharCode(chr1);
-
-      if (enc3 != 64) {
-         output = output + String.fromCharCode(chr2);
-      }
-      if (enc4 != 64) {
-         output = output + String.fromCharCode(chr3);
-      }
-   } while (i < input.length);
-
-   return output;
-}
-
-/***** From jsolait library: lgpl license *****************/
-
-// LZW-compress a string
-function lzw_encode(s) {
-    var dict = {};
-    var data = (s + "").split("");
-    var out = [];
-    var currChar;
-    var phrase = data[0];
-    var code = 256;
-    for (var i=1; i<data.length; i++) {
-        currChar=data[i];
-        if (dict[phrase + currChar] != null) {
-            phrase += currChar;
-        }
-        else {
-            out.push(phrase.length > 1 ? dict[phrase] : phrase.charCodeAt(0));
-            dict[phrase + currChar] = code;
-            code++;
-            phrase=currChar;
-        }
-    }
-    out.push(phrase.length > 1 ? dict[phrase] : phrase.charCodeAt(0));
-    for (var i=0; i<out.length; i++) {
-        out[i] = String.fromCharCode(out[i]);
-    }
-    return out.join("");
-}
-
-// Decompress an LZW-encoded string
-function lzw_decode(s) {
-    var dict = {};
-    var data = (s + "").split("");
-    var currChar = data[0];
-    var oldPhrase = currChar;
-    var out = [currChar];
-    var code = 256;
-    var phrase;
-    for (var i=1; i<data.length; i++) {
-        var currCode = data[i].charCodeAt(0);
-        if (currCode < 256) {
-            phrase = data[i];
-        }
-        else {
-           phrase = dict[currCode] ? dict[currCode] : (oldPhrase + currChar);
-        }
-        out.push(phrase);
-        currChar = phrase.charAt(0);
-        dict[code] = oldPhrase + currChar;
-        code++;
-        oldPhrase = phrase;
-    }
-    return out.join("");
-}
-
-
 })();
+
