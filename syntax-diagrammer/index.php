e44798eae18dca7abfc996b66087718d12bf5029
Weston Ruter <westonruter@gmail.com>
2009-11-12 21:53:56 -0800

    Working up the UI some more

--- syntax-diagrammer/index.php
+++ syntax-diagrammer/index.php
@@ -2,14 +2,14 @@
 <html>
 	<head>
 		<meta charset="utf8">
-		<title>HPSG Diagrammer with SVG Tree Drawer</title>
+		<title>Syntax Tree Diagrammer with SVG Tree Drawer</title>
 		<script src="../svg-tree-drawer.js"></script>
 		<link rel="stylesheet" href="style.css" type="text/css" />
 	</head>
 	<body>
 		<hgroup>
 			<h2><a href="http://weston.ruter.net/projects/svg-tree-drawer/">SVG Tree Drawer</a>:</h2>
-			<h1>Syntax Diagrammer</h1>
+			<h1>Syntax Tree Diagrammer</h1>
 			<p>With support for <abbr title="Head-driven phrase structure grammar">HPSG</abbr></p>
 		</hgroup>
 		
@@ -19,76 +19,6 @@
 			<p id="treePlaceholder"><em>Loading tree…</em></p>
 		</div>
 		
-		<script>
-		/**
-		 * Take a DOM element and its children and convert it into a data
-		 * structure acceptable as input for TreeDrawer. Usually the DOM element
-		 * will be a hierarchical UL or an OL, but any DOM tree is acceptable as
-		 * long as its first node is text, and it optionally is followed by
-		 * further elements which serve as child nodes.
-		 * @param el {DOMElement}
-		 * @returns {Object} Input suitable for TreeDrawer data structure
-		 */
-		//parseDomTreeForTreeDrawer
-		
-		/**
-		 * Parse HTML list item and its nested lists into a data structure
-		 * acceptable as input for TreeDrawer. First node of an LI element
-		 * must be a text node, and the child nodes are child elements of a
-		 * subsequent UL or OL element.
-		 * @param el {DOMElement}
-		 * @returns {Object} Input suitable for TreeDrawer data structure
-		 * @todo Implementation would be much nicer if querySelectorAll had scope
-		 */
-		function parseListItemForTreeDrawer(el){
-			var node = {
-				label:'',
-				children:[]
-			};
-			if(el.nodeName.toLowerCase() != 'li')
-				throw Error('Expected a list item (LI) element');
-			
-			if(!el.firstChild)
-				return node;
-				
-			//Get the label
-			var labelNode = el.firstChild;
-			while(labelNode){
-				if(labelNode.nodeType == 1/*Element*/){
-					if(/^(ol|ul)$/i.test(labelNode.nodeName))
-						break;
-					if(typeof labelNode.value == 'string')
-						node.label += labelNode.value;
-					else
-						node.label += labelNode.textContent;
-				}
-				else if(labelNode.nodeType == 3/*Text*/){
-					node.label += labelNode.data;
-				}
-				labelNode = labelNode.nextSibling;
-			}
-			node.label = node.label.replace(/^\s+|\s+$/g, '').replace(/\s+/g, ' ');
-			
-			//Get the first UL or OL
-			var list = el.firstChild;
-			while(list = list.nextSibling){
-				if(list.nodeType == 1/*Element*/ && /^(ol|ul)$/i.test(list.nodeName)){
-					var li = list.firstChild;
-					while(li){
-						if(li.nodeType == 1/*Element*/ && li.nodeName.toLowerCase() == 'li'){
-							node.children.push(parseListItemForTreeDrawer(li));
-						}
-						li = li.nextSibling;
-					}
-					break;
-				}
-			}
-			return node;
-		}
-		//NOTE: We could do tags like
-		// @1[ no]
-		</script>
-		
 		<p>You may modify the tree above by editing the following hierarchical list:</p>
 		<!--
 		@todo: Upon clicking a node, set the background color of the corresponding node in the tree above.
@@ -96,58 +26,31 @@
 		@todo: We need to be able to add new nodes!
 		-->
 		<ul id="ui">
-			<li><textarea>S[phrase HEAD=@1 VAL=[COMPS=&lt;&gt; SPR=&lt;&gt;]]</textarea>
-				<ul>
-					<li><textarea>@2N[word HEAD=[noun AGR=@3] VAL=[ COMPS=&lt;&gt; SPR=&lt;&gt; ] ]</textarea><ul>
-						<li><textarea>They</textarea></li>
-					</ul></li>
-					<li><textarea>VP[phrase HEAD=@1[verb AGR=@3 VAL=@4[ COMPS=&lt;&gt; SPR=&lt;@2&gt; ]]]</textarea>
-						<ul>
-							<li><textarea>V[word   HEAD=@1[verb AGR=[plural NUM=pl PER=3rd] ]  VAL=@4[ COMPS=&lt;@5&gt;  SPR=&lt;@2&gt; ]  ]</textarea><ul>
-								<li><textarea>seemed</textarea></li>
-							</ul></li>
-							<li><textarea>@5A[word HEAD=@7[adj]  VAL=@4[ COMPS=&lt;&gt; SPR=&lt;&gt; ]]</textarea>
-								<ul>
-									<li><textarea>close</textarea></li>
-								</ul>
-							</li>
-						</ul>
-					</li>
-				</ul>
-			</li>
+			<!--<li><textarea>S[phrase HEAD=@1 VAL=[COMPS=&lt;&gt; SPR=&lt;&gt;]]</textarea>-->
+			<!--	<ul>-->
+			<!--		<li><textarea>@2N[word HEAD=[noun AGR=@3] VAL=[ COMPS=&lt;&gt; SPR=&lt;&gt; ] ]</textarea><ul>-->
+			<!--			<li><textarea>They</textarea></li>-->
+			<!--		</ul></li>-->
+			<!--		<li><textarea>VP[phrase HEAD=@1[verb AGR=@3 VAL=@4[ COMPS=&lt;&gt; SPR=&lt;@2&gt; ]]]</textarea>-->
+			<!--			<ul>-->
+			<!--				<li><textarea>V[word   HEAD=@1[verb AGR=[plural NUM=pl PER=3rd] ]  VAL=@4[ COMPS=&lt;@5&gt;  SPR=&lt;@2&gt; ]  ]</textarea><ul>-->
+			<!--					<li><textarea>seemed</textarea></li>-->
+			<!--				</ul></li>-->
+			<!--				<li><textarea>@5A[word HEAD=@7[adj]  VAL=@4[ COMPS=&lt;&gt; SPR=&lt;&gt; ]]</textarea>-->
+			<!--					<ul>-->
+			<!--						<li><textarea>close</textarea></li>-->
+			<!--					</ul>-->
+			<!--				</li>-->
+			<!--			</ul>-->
+			<!--		</li>-->
+			<!--	</ul>-->
+			<!--</li>-->
 		</ul>
-		<script>
-		var data = parseListItemForTreeDrawer(document.querySelector('#ui > li'));
-		//data.extended = true;
+		<button type="reset">Start Over</button>
 		
-		var tree = new TreeDrawer(
-			document.getElementById('treePlaceholder'),
-			data
-		);
-		tree.draw();
-		
-		//Redraw the tree when the form fields change
-		document.getElementById('ui').addEventListener('change', function(e){
-			var treeContainer = document.getElementById('treeContainer');
-			treeContainer.style.minHeight = tree.svgElement.height.baseVal.value + 'px';
-			var data = parseListItemForTreeDrawer(document.querySelector('#parseTreeList > li'));
-			tree.draw(data);
-			treeContainer.style.minHeight = '';
-		}, false);
-		
-		//parseDomTreeForTreeDrawer(document.getElementById('parseTreeList').getElementsByTagName('li')[0]);
-		
-		//The “indent” and “outdent” execCommand will actually indent the entire
-		//contenteditable region! Workaround: check to make sure your selection is not
-		//directly parented by your edit region before executing this command. For the
-		//moment the following is working for me: Before an indent/outdent, I
-		//execCommand(“formatblock”, null, “p”) if there was no good parent already, in
-		//order to wrap the current selection or back in a <p>.
-		
-		
-		</script>
-		<!-- @todo: Make the  -->
-		
+		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.3/jquery.js"></script>
+		<script src="helpers.js"></script>
+		<script src="main.js"></script>
 		<hr>
 		<footer>
 			<address><a href="http://weston.ruter.net/" rel="author">Weston Ruter</a></address>
---
+++ syntax-diagrammer/main.js
@@ -0,0 +1,172 @@
+/**
+ * Syntax Tree Diagrammer
+ * Copyright: 2009, Weston Ruter <http://weston.ruter.net/>. GPL license.
+ *  
+ * GNU General Public License, Free Software Foundation <http://creativecommons.org/licenses/GPL/2.0/>
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *  
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *  
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+
+
+/**
+ * This tree data is displayed in the case that their is no existing user data
+ * provided in the URL fragment or in DOM storage.
+ */
+var defaultTreeData = {
+	label:'S[phrase HEAD=@1 VAL=[COMPS=<> SPR=<>]]',
+	children:[
+		{label:'@2N[word HEAD=[noun AGR=@3] VAL=[ COMPS=<> SPR=<> ] ]', children:[
+			{label:'They'}
+		]},
+		{label:'VP[phrase HEAD=@1[verb AGR=@3 VAL=@4[ COMPS=<> SPR=<@2> ]]]', children:[
+			{label:'V[word   HEAD=@1[verb AGR=[plural NUM=pl PER=3rd] ]  VAL=@4[ COMPS=<@5>  SPR=<@2> ]  ]', children:[
+				{label:'seemed'}
+			]},
+			{label:'@5A[word HEAD=@7[adj]  VAL=@4[ COMPS=<> SPR=<> ]]', children:[
+				{label:'close'}
+			]}
+		]}
+	]
+};
+
+var initialTreeData;
+
+
+/**
+ * Initialize the application
+ */
+jQuery(function(){
+	
+	//@todo Parse any data located in the URL fragment identifier or DOM storage
+	initialTreeData = defaultTreeData;
+	
+	var $ui = $('#ui');
+	$ui.empty();
+
+	//Draw the tree with the initial data
+	var tree = new TreeDrawer(
+		document.getElementById('treePlaceholder'),
+		initialTreeData
+	);
+	tree.draw();
+	
+	//Reset button
+	$('button[type=reset]').click(function(){
+		tree.draw(initialTreeData);
+		$ui.empty().append(constructInterfaceFromTreeNode(initialTreeData));
+		window.scrollTo(0,0);
+	});
+	
+	//Set up the user interface with the initial data
+	$ui.append(constructInterfaceFromTreeNode(initialTreeData));
+	
+	
+	//Redraw the tree when the form fields change
+	$('#ui textarea')
+		.live('change', function(e){
+			var container = tree.svgElement.parentNode;
+			container.style.minHeight = tree.svgElement.height.baseVal.value + 'px';
+			var data = parseListItemForTreeDrawer($ui.find('>li:first')[0]);
+			tree.draw(data);
+			container.style.minHeight = '';
+		})
+		.live('focus', function(e){
+			
+		})
+		.live('blur', function(e){
+			
+		});
+	
+	//$('ui').addEventListener('change', function(e){
+	//	var treeContainer = document.getElementById('treeContainer');
+	//	treeContainer.style.minHeight = tree.svgElement.height.baseVal.value + 'px';
+	//	var data = parseListItemForTreeDrawer($('#ui > li:first')[0]);
+	//	tree.draw(data);
+	//	treeContainer.style.minHeight = '';
+	//}, false);
+});
+
+
+/**
+ * Convert HPSG shorthand into XML which can then be transformed using XSLT
+ */
+function parseTextIntoHPSGXML(text){
+	
+}
+
+
+
+/**
+ * Take the tree data and construct a hierarchical list with textareas
+ */
+function constructInterfaceFromTreeNode(treeNode){
+	var li = document.createElement('li');
+	var textarea = document.createElement('textarea');
+	textarea.value = treeNode.label;
+	li.appendChild(textarea);
+	if(treeNode.children && treeNode.children.length){
+		var ul = document.createElement('ul');
+		$(treeNode.children).each(function(){
+			ul.appendChild(constructInterfaceFromTreeNode(this));
+		});
+		li.appendChild(ul);
+	}
+	return li;
+}
+
+
+/**
+ * Parse HTML list item and its nested lists into a data structure
+ * acceptable as input for TreeDrawer. First node of an LI element
+ * must be a text node, and the child nodes are child elements of a
+ * subsequent UL or OL element.
+ * @param el {DOMElement}
+ * @returns {Object} Input suitable for TreeDrawer data structure
+ * @todo Implementation would be much nicer if querySelectorAll had scope
+ */
+function parseListItemForTreeDrawer(el){
+	var node = {
+		label:'',
+		children:[]
+	};
+	if(!$(el).is('li'))
+		throw Error('Expected a list item (LI) element');
+	
+	if(!el.firstChild)
+		return node;
+		
+	//Get the label
+	var labelNode = el.firstChild;
+	while(labelNode){
+		if(labelNode.nodeType == 1/*Element*/){
+			if(/^(ol|ul)$/i.test(labelNode.nodeName))
+				break;
+			if(typeof labelNode.value == 'string')
+				node.label += labelNode.value;
+			else
+				node.label += labelNode.textContent;
+		}
+		else if(labelNode.nodeType == 3/*Text*/){
+			node.label += labelNode.data;
+		}
+		labelNode = labelNode.nextSibling;
+	}
+	node.label = node.label.replace(/^\s+|\s+$/g, '').replace(/\s+/g, ' ');
+	
+	//Get the first UL or OL
+	$(el).find('> ul > li, > ol > li').each(function(){
+		node.children.push(parseListItemForTreeDrawer(this));
+	});
+	return node;
+}
