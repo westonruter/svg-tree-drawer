a9f0db1a5f3664e5251653a89141ba4c613e96ac
Weston Ruter <westonruter@gmail.com>
2009-11-21 23:04:38 -0800

    More work on hpsg.js

--- syntax-diagrammer/hpsg.js
+++ syntax-diagrammer/hpsg.js
@@ -57,50 +57,124 @@ function convertTextToHPSG(text){
 	
 	var ns = 'http://weston.ruter.net/ns/hpsg';
 	var doc = document.implementation.createDocument('', '', null);
-	var parentNode = doc;
 	
-	var reTok = new RegExp( [
-		'(\\s+)', //1: whitespace
-		'([A-Z]+)', //2: abbr
-		'([a-z][0-9]*)', //3: index
-		'([0-9]+)' //4: tag
-		//…
-	].join('|'), 'g');
+	var reTok = new RegExp( '^(?:' + [
+		'(\\s+|,)', //1: whitespace
+		'([A-Z]+(?!\s*[:=]))', //2: abbr
+		'([a-z][0-9]*\\b)', //3: index
+		'([0-9]+)', //4: tag
+		'(\\[)', //5
+		'(\\])', //6
+		'([A-Z]+)\\b\s*[:=]', //7 attribute name 
+		'(<)', //8
+		'(>)', //9
+		'(…)', //10
+		'(\\b[a-z][a-z]+\\b)' //11 attribute value
+	].join('|') + ')');
 	
-	var index;
-	var abbr;
-	var tag;
-	var lastIndex = 0;
-	while(m = reTok.exec(text)){
-		lastIndex = reTok.lastIndex;
+	console.info(text)
+	
+	var index, label, tag, name, value;
+	var pos = 0;
+	//var tokens = [];
+	//var avmStack = [], listStack = [];
+	var curParent = doc;
+	while(m = reTok.exec(text.substr(pos))){
+		//lastIndex = reTok.lastIndex;
+		pos += m[0].length;
 		
+		console.info(m[0])
+		
+		var isEnd = (pos == text.length);
+		//var isTokenEnd = (m[1] || pos == text.length);
+		
+		//Gather abbr/index/tag
 		if(m[2])      abbr = m[2];
 		else if(m[3]) index = m[3];
 		else if(m[4]) tag = m[4];
 		
-		//Token is made!
-		if(m[1] || lastIndex == text.length){
-			var el;
-			
-			//Construct HPSG AVM node
-			if(abbr || index || tag){
-				el = doc.createElementNS(ns, 'avm');
-				if(abbr)
-					el.setAttribute('abbr', abbr);
-				if(index)
-					el.setAttribute('index', index);
-				if(tag)
-					el.setAttribute('tag', tag);
-				doc.appendChild(el);
-			}
-			
+		//Opening AVM: [ or AVM that doesn't have brackets
+		else if(m[5] || isEnd){ //(m[1] || pos == text.length)
+			console.warn('(m[5] || isEnd')
+			var el = document.createElementNS(ns, 'avm');
+			if(abbr) el.setAttribute('abbr', abbr);
+			if(index) el.setAttribute('index', index);
+			if(tag)   el.setAttribute('tag', tag);
+			curParent = curParent.appendChild(el);
 			index = abbr = tag = null;
 		}
+		//AVM attribute name followed by separator (:|=)
+		else if(m[7]){
+			console.warn('m[7]')
+			el = document.createElementNS(ns, 'attr');
+			el.setAttribute('name', m[7]);
+			curParent = curParent.appendChild(el);
+		}
+		//Closing AVM: ]
+		else if(m[6]){
+			console.warn('m[6]', index)
+			//Bump up out of attr
+			if(curParent.localName == 'attr'){
+				if(abbr)
+					curParent.appendChild(document.createTextNode(abbr));
+				curParent = curParent.parentNode.parentNode;
+			}
+			else
+				curParent = curParent.parentNode; //we're probably in an empty AVM
+		}
+		
+		//Opening list: <
+		else if(m[8]){
+			console.warn('m[8]')
+			var el = document.createElementNS(ns, 'list');
+			if(tag) el.setAttribute('tag', tag);
+			tag = null;
+			curParent = curParent.appendChild(el);
+		}
+		//Closing list: >
+		else if(m[9]){
+			console.warn('m[9]')
+			curParent = curParent.parentNode;
+		}
+		//Ellipsis: …
+		else if(m[10]){
+			console.warn('m[10]')
+			el = document.createElementNS(ns, 'ellipsis');
+			curParent.appendChild(el);
+			//curParent = curParent.parentNode;
+			
+			
+		}
+		
+		//
+		//
+		//
+		////Token is made!
+		//if(m[1] || pos == text.length){
+		//	var el;
+		//	
+		//	//Construct HPSG AVM node
+		//	if(label || index || tag){
+		//		el = doc.createElementNS(ns, 'avm');
+		//		if(abbr)
+		//			el.setAttribute('abbr', abbr);
+		//		if(index)
+		//			el.setAttribute('index', index);
+		//		if(tag)
+		//			el.setAttribute('tag', tag);
+		//		//doc.appendChild(el);
+		//		parentNode = el;
+		//	}
+		//	else {
+		//		
+		//	}
+		//	index = abbr = tag = null;
+		//}
 	}
-	if(lastIndex != text.length)
-		throw Error("Unexpected character: " + text.substr(lastIndex));
+	if(pos != text.length){
+		throw Error("Unexpected character: " + text.substr(pos));
+	}
 	//catch parseError!
-	
 	
 	return doc;
 }
